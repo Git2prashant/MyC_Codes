8554d6b828cdee7036fbc94590cdbecfafb3cfc6
Prashant <84545907+Git2prashant@users.noreply.github.com>
2025-11-30 20:39:24 +0530

    Minor Fixes
    Fixed minor issues & added few more comments

--- main.c
+++ main.c
@@ -4,22 +4,27 @@
 
 
 int main() {
+    
+    //To Check whether a person is eligible for getting Student discount or not.
+    
+    //All Kind of Students will be getting 25%,50%,75% OFF for any purchase.
 
-    //My First Code push on Github
+
+    //My First Code push on Github !
+
     char designation[10]="";
     float price = 100;
     float total;
     char percent='%';
     bool isSenior = true;
 
-    printf("Orginal Price is:%.2f\n",price);
+    printf("Orginal Price is $%.2f\n",price);
     printf("Enter you Designation - Student(ST)/Senior(SR)/Both(B):");
     fgets(designation,sizeof(designation),stdin);
     designation[strlen(designation) - 1] = '\0';
 
     if (strcmp(designation,"ST")==0 || strcmp(designation,"st")==0 || strcmp(designation,"sT")==0 || strcmp(designation,"St")==0){
-    }                //Nested Loops
-        if (isSenior) {
+    
             
     printf("You're a deserving Student & you'll get a 25%c OFF.\n",percent);
     total = price-(price*0.25);
@@ -28,14 +33,14 @@ int main() {
 
     else if (strcmp(designation,"SR")==0 || strcmp(designation,"sr")==0 || strcmp(designation,"sR")==0 || strcmp(designation,"Sr")==0)
     {
-    printf("You're a Senior & you'll get a 50%c OFF\n",percent);
+    printf("You're a Senior & you'll get a 50%c OFF.\n",percent);
     total = price*0.5 ;
     printf("Your final price will be: $%.2f",total);
     }
 
     else if (strcmp(designation,"B")==0 || strcmp(designation,"b")==0)
     {
-        printf("Amazing Man , You're both of them\n");
+        printf("Amazing Man,You're both of them & you'll get a total 75%c of discount.\n",percent);
         total = price-(price*0.75);
         printf("Your final price will be: $%.2f",total);
     }
